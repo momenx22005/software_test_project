087529af182768328c7b55063bdb0b1cf6cbdf42
momenx22005 <s12324285@stu.najah.edu>
2026-02-21 20:24:55 +0200

    Initial commit

---
+++ src/main/java/com/test_project/App.java
@@ -0,0 +1,22 @@
+package com.test_project;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) {
+        // Create frame
+        JFrame frame = new JFrame("Test GUI");
+        frame.setSize(100, 320);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        // Create button
+        JButton button = new JButton("Click me");
+        button.setBounds(100, 70, 100, 30); // x, y, width, height
+        frame.add(button);
+
+        // Show frame
+        frame.setVisible(true);
+    }
+}
